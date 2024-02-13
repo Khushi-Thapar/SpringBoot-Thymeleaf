c463b627abc792c582d49341b657931950676b94
Khushi Thapar (CW) <Khushi.Thapar@jda.com>
2024-02-13 09:56:19 +0530

    Thymeleaf example project

---
+++ src/main/java/com/example/demo/ThymeleafSpringBootdemo11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringBootdemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringBootdemo11Application.class, args);
+	}
+
+}
